4ebef07bdf98e7146e9940c4347a19c68abcee16
rafaq <rafaq@DESKTOP-OQS600R>
2021-07-10 19:53:30 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+namespace ConsoleApp1
+{ 
+    interface i1
+    {
+        void InterFaceMethod();
+    }
+    interface i2
+    {
+        void InterFaceMethod();
+    }
+    
+    class Program : i1,i2
+    {
+        static void Main(string[] args)
+        {
+
+            Program p = new Program();
+            p.InterFaceMethod();
+            ((i2)p).InterFaceMethod();
+        }
+
+        public void InterFaceMethod()
+        {
+            Console.WriteLine("i1 interface method");
+
+        }
+        void i2.InterFaceMethod()
+        {
+            Console.WriteLine("i2 interface method");
+
+        }
+    }
+        
+}
